8dcc94d4d8b5e09c188db1161df7671265d308a3
Monika <mja@corcode.com>
2016-05-16 10:13:25 +0200

    kolos1

--- Program.cs
+++ Program.cs
@@ -1,59 +1,72 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication4
+namespace Fish
 {
     class Program
     {
-        private int[] A;
-        private int[] B;
         /// <summary>
-        /// porównuje czy ryby płyna w tym samym kierunku 
+        /// Metoda fish zwraca liczbe ryb ktore przezyly
         /// </summary>
-        /// <param name="firstIndex"></param>
-        /// <param name="secondIndex"></param>
+        /// <param name="a">tablica z wielkosciami ryb</param>
+        /// <param name="b">tablica z kierunkiem ryb</param>
         /// <returns></returns>
-        public bool CompareFishDirection(int firstIndex, int secondIndex)
+        public static int fish(int[] a, int[] b)
         {
-            return true;
-        }
-        /// <summary>
-        /// Tworzy tablice z rybami
-        /// </summary>
-        public void InitFishArray()
-        {
-
-        }
-        /// <summary>
-        /// Tworzy tablice z kierunkiem w którym płyna ryby
-        /// </summary>
-        public void InitDirectionArray()
-        {
-
-        }
-        /// <summary>
-        /// Sprawdza czy dana ryba moze zjesc inną
-        /// </summary>
-        /// <param name="fish"></param>
-        /// <param name="target"></param>
-        /// <returns></returns>
-        public bool CanEatFish(int fish, int target)
-        {
-            return true;
-        }
-        /// <summary>
-        /// Zjedzenie ryby
-        /// </summary>
-        /// <param name="killer"></param>
-        /// <param name="target"></param>
-        public void Killfish(int killer, int target)
-        {
+            Stack<int> zywe = new Stack<int>();
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (zywe.Count == 0)
+                    zywe.Push(i);
+                else
+                {
+                    while (zywe.Count != 0
+                        && b[i] - b[zywe.Peek()] == -1
+                        && a[i] > a[zywe.Peek()])
+                    {
+                        zywe.Pop();
+                    }
+                    if (zywe.Count != 0)
+                    {
+                        if (b[i] - b[zywe.Peek()] != -1)
+                            zywe.Push(i);
+                    }
+                    else
+                    {
+                        zywe.Push(i);
+                    }
+                }
+            }
+            return zywe.Count;
         }
         static void Main(string[] args)
         {
+            Random next = new Random();
+            Console.WriteLine("Podaj ilosc ryb: ");
+            int size = Console.Read();
+            int[] a = new int[size], b = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                a[i] = next.Next(101);
+                b[i] = next.Next(2);
+                Console.WriteLine("Wielkosc ryby {0} = {1}, kierunek = {2}", i, a[i], b[i]);
+            }
+            Console.WriteLine("Przezylo: {0}", fish(a, b));
+            Console.ReadKey();
+        }
+    }
+    [TestClass]
+    public class TestFishClass
+    {
+        [TestMethod]
+        public void TestFishMethod()
+        {
+            int[] a = { 4, 3, 2, 1, 5 }, b = { 0, 1, 0, 0, 0 };
+            Assert.AreEqual(2, Program.fish(a, b));
         }
     }
 }
