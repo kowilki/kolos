0733db66b16195cfd8b677a48e6d1412298bdb51
Monika <mja@corcode.com>
2016-05-16 10:01:55 +0200

    kolokwium1

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        private int[] A;
+        private int[] B;
+        /// <summary>
+        /// porównuje czy ryby płyna w tym samym kierunku 
+        /// </summary>
+        /// <param name="firstIndex"></param>
+        /// <param name="secondIndex"></param>
+        /// <returns></returns>
+        public bool CompareFishDirection(int firstIndex, int secondIndex)
+        {
+            return true;
+        }
+        /// <summary>
+        /// Tworzy tablice z rybami
+        /// </summary>
+        public void InitFishArray()
+        {
+
+        }
+        /// <summary>
+        /// Tworzy tablice z kierunkiem w którym płyna ryby
+        /// </summary>
+        public void InitDirectionArray()
+        {
+
+        }
+        /// <summary>
+        /// Sprawdza czy dana ryba moze zjesc inną
+        /// </summary>
+        /// <param name="fish"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public bool CanEatFish(int fish, int target)
+        {
+            return true;
+        }
+        /// <summary>
+        /// Zjedzenie ryby
+        /// </summary>
+        /// <param name="killer"></param>
+        /// <param name="target"></param>
+        public void Killfish(int killer, int target)
+        {
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
